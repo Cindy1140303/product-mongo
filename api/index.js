087a521224be4bb4862b994716f4cc1d50554e43
Cindy1140303 <cherry241132@gmail.com>
2025-11-14 06:33:56 +0800

    修正 Vercel API handler: 正確處理 serverless function 請求

--- api/index.js
+++ api/index.js
@@ -3,4 +3,11 @@
 
 const app = require('../server');
 
-module.exports = app;
+// Vercel 需要一個處理函數，而不是直接導出 app
+module.exports = async (req, res) => {
+  // 移除 /api 前綴，因為 Vercel 會自動處理
+  req.url = req.url.replace(/^\/api/, '') || '/';
+  
+  // 讓 Express 處理請求
+  return app(req, res);
+};
